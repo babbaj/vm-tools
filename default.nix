49a9f7e18c9bafbab2031c3640477f0986e9eead
Babbaj <babbaj45@gmail.com>
2021-07-16 19:36:29 -0400

    actually delete the tmp folder

--- default.nix
+++ default.nix
@@ -19,11 +19,11 @@ writeScriptBin "dump"
     rust=$(mktemp -d -t rust-XXXX)
     ${depotdownloader}/bin/depotdownloader -os windows -osarch 64 -app 252490 -filelist ${files} -dir $rust -username $STEAM_USER -password $STEAM_PASS
     if [[ $? -ne 0 ]] ; then 
-        rm -rf rust
+        rm -rf $rust
         exit $?
     fi
     ${dumper}/bin/Il2CppDumper $rust/GameAssembly.dll $rust/RustClient_Data/il2cpp_data/Metadata/global-metadata.dat ./
     err=$?
-    rm -rf rust
+    rm -rf $rust
     exit $err
 ''
