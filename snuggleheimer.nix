7e591e803fe520db1927a4e4bce4048f3fdab563
Babbaj <babbaj45@gmail.com>
2022-03-01 01:31:36 -0500

    update snuggleheimer

--- snuggleheimer.nix
+++ snuggleheimer.nix
@@ -2,41 +2,38 @@
 
 with pkgs;
 stdenv.mkDerivation {
-    pname = "snuggleheimer";
-    version = "nightly";
+  pname = "snuggleheimer";
+  version = "nightly";
 
-    /*src = builtins.fetchGit {
-        url = "ssh://git@github.com/babbaj/snuggleheimer.git";
-        ref = "snuggle";
-        rev = "8bc24aaab0f5a717aaa75b8b089529e28e609e01";
-        submodules = true;
-    };*/
-    inherit src;
+  inherit src;
 
-    sourceRoot = "source/client";
+  sourceRoot = "source/client";
 
-    nativeBuildInputs = [ cmake pkg-config ];
+  nativeBuildInputs = [ cmake pkg-config ];
 
-    buildInputs = with xorg; [
-        libGL
-        freefont_ttf
-        spice-protocol
-        expat
-        libbfd
-        nettle
-        fontconfig
-        libffi
-        libxkbcommon
-        libXi
-        libXScrnSaver
-        libXinerama
-        libXcursor
-        libXpresent
-        libXext
-        libXrandr
-        #wayland
-        #wayland-protocols
-    ];
+  buildInputs = with xorg; [
+    libGL
+    freefont_ttf
+    spice-protocol
+    expat
+    libbfd
+    nettle
+    fontconfig
+    libffi
+    libxkbcommon
+    libXi
+    libXScrnSaver
+    libXinerama
+    libXcursor
+    libXpresent
+    libXext
+    libXrandr
+    #wayland
+    #wayland-protocols
+    pipewire.dev
+    libpulseaudio
+    libsamplerate
+  ];
 
-    cmakeFlags = [ "-DOPTIMIZE_FOR_NATIVE=OFF" "-DENABLE_WAYLAND=no" ];
+  cmakeFlags = [ "-DOPTIMIZE_FOR_NATIVE=OFF" "-DENABLE_WAYLAND=no" ];
 }
